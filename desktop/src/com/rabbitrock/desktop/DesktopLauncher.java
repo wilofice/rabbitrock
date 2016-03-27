ffd09d6dd0127bb0ea351b8ca5442f0f43b88e51
wilofice <wilofice@gmail.com>
2016-03-27 16:46:49 +0100

    << Fichier de configuration générés par libgdx >>

---
+++ desktop/src/com/rabbitrock/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rabbitrock.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rabbitrock.RabbitRockMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RabbitRockMain(), config);
+	}
+}
