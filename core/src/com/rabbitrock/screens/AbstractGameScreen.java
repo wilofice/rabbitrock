4098b73e577ba7f14f994dabcdae04f4bfc08192
Omarx01 <omar.attaf@gmail.com>
2016-04-25 08:51:17 +0100

    sans importance :

--- core/src/com/rabbitrock/screens/AbstractGameScreen.java
+++ core/src/com/rabbitrock/screens/AbstractGameScreen.java
@@ -2,7 +2,7 @@ package com.rabbitrock.screens;
 import com.badlogic.gdx.Game; 
 import com.badlogic.gdx.Screen; 
 import com.badlogic.gdx.assets.AssetManager;
-import com.packtpub.libgdx.canyonbunny.game.Assets;
+import com.rabbitrock.game.Assets;
 
 public abstract class AbstractGameScreen implements Screen {
 	protected Game gamen;
