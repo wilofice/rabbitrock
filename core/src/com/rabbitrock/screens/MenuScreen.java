c3501b89d98f35c737d3bf1c6d43e0f396fb1590
wilofice <wilofice@gmail.com>
2016-05-10 10:43:44 +0100

    Correction des classes GameScreen et MenuScreen

--- core/src/com/rabbitrock/screens/MenuScreen.java
+++ core/src/com/rabbitrock/screens/MenuScreen.java
@@ -1,5 +1,36 @@
 package com.rabbitrock.screens;
 
-public class MenuScreen {
+import com.badlogic.gdx.Game;
+
+public class MenuScreen extends AbstractGameScreen {
+	 private static final String TAG = MenuScreen.class.getName();
+	  public MenuScreen (Game game) {   
+		  super(game); 
+	  }
+	@Override
+	public void render(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
