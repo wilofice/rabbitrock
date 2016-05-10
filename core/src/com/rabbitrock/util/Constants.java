52b1d17c3ccce744d418f7fc8b9cd36e080dcb4e
wilofice <wilofice@gmail.com>
2016-05-10 03:13:42 +0100

    Commit by Généreux branch Meriam

--- core/src/com/rabbitrock/util/Constants.java
+++ core/src/com/rabbitrock/util/Constants.java
@@ -6,5 +6,8 @@ public class Constants {
 		
 		// Visible game world in 5 meters tall
 		public static final float VIEWPORT_HEIGHT = 5.0f;
-
+		// Location of description file for texture atlas
+		public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
+		// Location of image file for level 01
+		public static final String LEVEL_01 = "levels/level-01.png";
 }
