ba35edb334b5199ccc5f2bb115581a5f58d3a12a
wilofice <wilofice@gmail.com>
2016-05-21 06:05:36 +0100

    Commit

--- core/src/com/rabbitrock/screens/GameScreen.java
+++ core/src/com/rabbitrock/screens/GameScreen.java
@@ -1,14 +1,89 @@
 package com.rabbitrock.screens;
+<<<<<<< Updated upstream
 
+||||||| merged common ancestors
+
+import com.badlogic.gdx.Game;
+=======
+import com.badlogic.gdx.Game;
+>>>>>>> Stashed changes
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.rabbitrock.game.WorldController;
+import com.rabbitrock.game.WorldController; 
 import com.rabbitrock.game.WorldRenderer;
 import com.rabbitrock.util.GamePreferences;
 
 public class GameScreen extends AbstractGameScreen {
+<<<<<<< Updated upstream
+||||||| merged common ancestors
+	 private static final String TAG = GameScreen.class.getName();
+	 
+	 private WorldController worldController;
+	 private WorldRenderer worldRenderer;
+	 
+	 private boolean paused;
+	 
+	 public GameScreen (Game game) {   
+		  super(game);  
+	  }	 
 
+@Override  public void render (float deltaTime) { 
+	// Do not update game world when paused. 
+	if (!paused) {   
+		// Update game world by the time that has passed    
+		// since last rendered frame.    
+		worldController.update(deltaTime);  
+		}  
+	// Sets the clear screen color to: Cornflower Blue  
+	Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f,0xed / 
+255.0f, 0xff / 255.0f);  
+	// Clears the screen 
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  
+	// Render game world to screen    
+	worldRenderer.render(); 
+	}  
+@Override 
+	public void resize (int width, int height) {  
+		worldRenderer.resize(width, height); 
+		
+}
+=======
+	 private static final String TAG = GameScreen.class.getName();
+	 
+	 private WorldController worldController;
+	 private WorldRenderer worldRenderer;
+	 
+	 private boolean paused;
+	 
+	 public GameScreen (Game game) {   
+		  super(game);  
+	  }	 
+
+@Override 
+public void render (float deltaTime) { 
+	// Do not update game world when paused. 
+	if (!paused) {   
+		// Update game world by the time that has passed    
+		// since last rendered frame.    
+		worldController.update(deltaTime);  
+		}  
+	// Sets the clear screen color to: Cornflower Blue  
+	Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f,0xed / 
+255.0f, 0xff / 255.0f);  
+	// Clears the screen 
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  
+	// Render game world to screen    
+	worldRenderer.render(); 
+	}  
+@Override 
+	public void resize (int width, int height) {  
+		worldRenderer.resize(width, height); 
+		
+}
+>>>>>>> Stashed changes
+
+<<<<<<< Updated upstream
 	private static final String TAG = GameScreen.class.getName();
 
 	private WorldController worldController;
@@ -40,7 +115,32 @@ public class GameScreen extends AbstractGameScreen {
 	public void resize (int width, int height) {
 		worldRenderer.resize(width, height);
 	}
+||||||| merged common ancestors
+ @Override 
+	  public void show () {  
+		  worldController = new WorldController();
+		  worldRenderer = new WorldRenderer(worldController);
+		  Gdx.input.setCatchBackKey(true);
+}
+	  public void hide () {  
+		  worldRenderer.dispose();  
+		  Gdx.input.setCatchBackKey(false); 
+}
+=======
+ @Override 
+	  public void show () {  
+	 	  //GamePreferences.instance.load(); 
+		  worldController = new WorldController(game);
+		  worldRenderer = new WorldRenderer(worldController);
+		  Gdx.input.setCatchBackKey(true);
+}
+	  public void hide () {  
+		  worldRenderer.dispose();  
+		  Gdx.input.setCatchBackKey(false); 
+}
+>>>>>>> Stashed changes
 
+<<<<<<< Updated upstream
 	@Override
 	public void show () {
 		GamePreferences.instance.load();
@@ -73,3 +173,20 @@ public class GameScreen extends AbstractGameScreen {
 	}
 
 }
+||||||| merged common ancestors
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}}
+=======
+@Override  public void pause () {   
+	paused = true;
+}
+
+@Override  public void resume () { 
+	super.resume();  
+	// Only called on Android!   
+	paused = false;  }
+} 
+>>>>>>> Stashed changes
--- core/src/com/rabbitrock/util/Constants.java
+++ core/src/com/rabbitrock/util/Constants.java
@@ -15,6 +15,7 @@ public class Constants {
 
 		// Location of description file for texture atlas
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 		public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
 		public static final String TEXTURE_ATLAS_UI = "images/canyonbunny-ui.pack";
 		public static final String TEXTURE_ATLAS_LIBGDX_UI = "images/uiskin.atlas";
@@ -23,6 +24,11 @@ public class Constants {
 		public static final String SKIN_LIBGDX_UI = "images/uiskin.json";
 		public static final String SKIN_CANYONBUNNY_UI = "images/canyonbunny-ui.json";
 
+||||||| merged common ancestors
+		public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
+=======
+		public static final String TEXTURE_ATLAS_OBJECTS = "../android/assets/images/canyonbunny.pack";
+>>>>>>> Stashed changes
 ||||||| merged common ancestors
 		public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
 =======
@@ -37,12 +43,24 @@ public class Constants {
 		// Duration of feather power-up in seconds
 		public static final float ITEM_FEATHER_POWERUP_DURATION = 9;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 		// Delay after game over
 		public static final float TIME_DELAY_GAME_OVER = 3;
 
 		// Game preferences file
 		public static final String PREFERENCES = "canyonbunny.prefs";
+||||||| merged common ancestors
+=======
+		public static final String TEXTURE_ATLAS_UI = 
+				"images/canyonbunny-ui.pack"; 
+		public static final String TEXTURE_ATLAS_LIBGDX_UI =  
+				"images/uiskin.atlas"; 
+		// Location of description file for skins  
+		public static final String SKIN_LIBGDX_UI =    
+				"images/uiskin.json"; 
+		public static final String SKIN_CANYONBUNNY_UI =      "images/canyonbunny-ui.json"; 
+>>>>>>> Stashed changes
 ||||||| merged common ancestors
 =======
 		public static final String TEXTURE_ATLAS_UI = 
