c8425d7e0a9ad1482b183c0c59016f4dc5dc8f33
wilofice <wilofice@gmail.com>
2016-05-21 06:33:35 +0100

    Commit passager

--- core/src/com/rabbitrock/screens/GameScreen.java
+++ core/src/com/rabbitrock/screens/GameScreen.java
@@ -1,107 +1,14 @@
 package com.rabbitrock.screens;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-||||||| merged common ancestors
-
-import com.badlogic.gdx.Game;
-=======
-||||||| merged common ancestors
-
-=======
->>>>>>> Stashed changes
-||||||| merged common ancestors
-
-=======
->>>>>>> Stashed changes
-import com.badlogic.gdx.Game;
->>>>>>> Stashed changes
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.rabbitrock.game.WorldController; 
+import com.rabbitrock.game.WorldController;
 import com.rabbitrock.game.WorldRenderer;
 import com.rabbitrock.util.GamePreferences;
 
 public class GameScreen extends AbstractGameScreen {
-<<<<<<< Updated upstream
-||||||| merged common ancestors
-	 private static final String TAG = GameScreen.class.getName();
-	 
-	 private WorldController worldController;
-	 private WorldRenderer worldRenderer;
-	 
-	 private boolean paused;
-	 
-	 public GameScreen (Game game) {   
-		  super(game);  
-	  }	 
 
-<<<<<<< Updated upstream
-@Override 
-public void render (float deltaTime) { 
-	// Do not update game world when paused. 
-	if (!paused) {   
-		// Update game world by the time that has passed    
-		// since last rendered frame.    
-		worldController.update(deltaTime);  
-		}  
-	// Sets the clear screen color to: Cornflower Blue  
-	Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f,0xed / 
-255.0f, 0xff / 255.0f);  
-	// Clears the screen 
-	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  
-	// Render game world to screen    
-	worldRenderer.render(); 
-	}  
-@Override 
-	public void resize (int width, int height) {  
-		worldRenderer.resize(width, height); 
-		
-}
-=======
-	 private static final String TAG = GameScreen.class.getName();
-	 
-	 private WorldController worldController;
-	 private WorldRenderer worldRenderer;
-	 
-	 private boolean paused;
-	 
-	 public GameScreen (Game game) {   
-		  super(game);  
-	  }	 
-
-@Override 
-public void render (float deltaTime) { 
-||||||| merged common ancestors
-@Override  public void render (float deltaTime) { 
-=======
-@Override 
-public void render (float deltaTime) { 
->>>>>>> Stashed changes
-	// Do not update game world when paused. 
-	if (!paused) {   
-		// Update game world by the time that has passed    
-		// since last rendered frame.    
-		worldController.update(deltaTime);  
-		}  
-	// Sets the clear screen color to: Cornflower Blue  
-	Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f,0xed / 
-255.0f, 0xff / 255.0f);  
-	// Clears the screen 
-	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  
-	// Render game world to screen    
-	worldRenderer.render(); 
-	}  
-@Override 
-	public void resize (int width, int height) {  
-		worldRenderer.resize(width, height); 
-		
-}
->>>>>>> Stashed changes
-
-<<<<<<< Updated upstream
 	private static final String TAG = GameScreen.class.getName();
 
 	private WorldController worldController;
@@ -133,33 +40,7 @@ public void render (float deltaTime) {
 	public void resize (int width, int height) {
 		worldRenderer.resize(width, height);
 	}
-||||||| merged common ancestors
- @Override 
-	  public void show () {  
-	 	  //GamePreferences.instance.load(); 
-		  worldController = new WorldController(game);
-		  worldRenderer = new WorldRenderer(worldController);
-		  Gdx.input.setCatchBackKey(true);
-}
-	  public void hide () {  
-		  worldRenderer.dispose();  
-		  Gdx.input.setCatchBackKey(false); 
-}
-=======
- @Override 
-	  public void show () {  
-	 	  //GamePreferences.instance.load(); 
-		  worldController = new WorldController(game);
-		  worldRenderer = new WorldRenderer(worldController);
-		  Gdx.input.setCatchBackKey(true);
-}
-	  public void hide () {  
-		  worldRenderer.dispose();  
-		  Gdx.input.setCatchBackKey(false); 
-}
->>>>>>> Stashed changes
 
-<<<<<<< Updated upstream
 	@Override
 	public void show () {
 		GamePreferences.instance.load();
@@ -168,7 +49,6 @@ public void render (float deltaTime) {
 		Gdx.input.setCatchBackKey(true);
 	}
 
-<<<<<<< Updated upstream
 	@Override
 	public void hide () {
 		worldRenderer.dispose();
@@ -180,7 +60,6 @@ public void render (float deltaTime) {
 		paused = true;
 	}
 
-<<<<<<< Updated upstream
 	@Override
 	public void resume () {
 		super.resume();
@@ -194,54 +73,3 @@ public void render (float deltaTime) {
 	}
 
 }
-||||||| merged common ancestors
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}}
-=======
-@Override  public void pause () {   
-	paused = true;
-}
-
-@Override  public void resume () { 
-	super.resume();  
-	// Only called on Android!   
-	paused = false;  }
-} 
->>>>>>> Stashed changes
-||||||| merged common ancestors
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}}
-=======
-@Override  public void pause () {   
-	paused = true;
-}
-
-@Override  public void resume () { 
-	super.resume();  
-	// Only called on Android!   
-	paused = false;  }
-} 
->>>>>>> Stashed changes
-||||||| merged common ancestors
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}}
-=======
-@Override  public void pause () {   
-	paused = true;
-}
-
-@Override  public void resume () { 
-	super.resume();  
-	// Only called on Android!   
-	paused = false;  }
-} 
->>>>>>> Stashed changes
