1e813ae62e3db2c52b02e2f5a6c0495ee23c6ac1
wilofice <wilofice@gmail.com>
2016-05-21 04:08:36 +0100

    Commit by alahassa Correction des erreurs Premier test-OK Reste à ajouter la touche personnelle

--- core/src/com/rabbitrock/util/CameraHelper.java
+++ core/src/com/rabbitrock/util/CameraHelper.java
@@ -8,68 +8,70 @@ import com.rabbitrock.game.objects.AbstractGameObject;
 
 
 public class CameraHelper {
-	 private static final String TAG = CameraHelper.class.getName();
+	private static final String TAG = CameraHelper.class.getName();
 
-     private final float MAX_ZOOM_IN = 0.25f;
-     private final float MAX_ZOOM_OUT = 10.0f;
+	private final float MAX_ZOOM_IN = 0.25f;
+	private final float MAX_ZOOM_OUT = 10.0f;
+	private final float FOLLOW_SPEED = 4.0f;
 
-     private Vector2 position;
-     private float zoom;
-     private AbstractGameObject target;
+	private Vector2 position;
+	private float zoom;
+	private AbstractGameObject target;
 
-     public CameraHelper () {
-             position = new Vector2();
-             zoom = 1.0f;
-     }
+	public CameraHelper () {
+		position = new Vector2();
+		zoom = 1.0f;
+	}
 
-     public void update (float deltaTime) {
-             if (!hasTarget()) return;
+	public void update (float deltaTime) {
+		if (!hasTarget()) return;
 
-             position.x = target.position.x + target.origin.x;
-             position.y = target.position.y + target.origin.y;
-     }
+		position.lerp(target.position, FOLLOW_SPEED * deltaTime);
+		
+		// Prevent camera from moving down too far
+		position.y = Math.max(-1f, position.y);
+	}
 
-     public void setPosition (float x, float y) {
-             this.position.set(x, y);
-     }
+	public void setPosition (float x, float y) {
+		this.position.set(x, y);
+	}
 
-     public Vector2 getPosition () {
-             return position;
-     }
+	public Vector2 getPosition () {
+		return position;
+	}
 
-     public void addZoom (float amount) {
-             setZoom(zoom + amount);
-     }
+	public void addZoom (float amount) {
+		setZoom(zoom + amount);
+	}
 
-     public void setZoom (float zoom) {
-             this.zoom = MathUtils.clamp(zoom, MAX_ZOOM_IN, MAX_ZOOM_OUT);
-     }
+	public void setZoom (float zoom) {
+		this.zoom = MathUtils.clamp(zoom, MAX_ZOOM_IN, MAX_ZOOM_OUT);
+	}
 
-     public float getZoom () {
-             return zoom;
-     }
+	public float getZoom () {
+		return zoom;
+	}
 
-     public void setTarget (AbstractGameObject target) {
-             this.target = target;
-     }
+	public void setTarget (AbstractGameObject target) {
+		this.target = target;
+	}
 
-     public AbstractGameObject getTarget () {
-             return target;
-     }
+	public AbstractGameObject getTarget () {
+		return target;
+	}
 
-     public boolean hasTarget () {
-             return target != null;
-     }
+	public boolean hasTarget () {
+		return target != null;
+	}
 
-     public boolean hasTarget (Sprite target) {
-             return hasTarget() && this.target.equals(target);
-     }
+	public boolean hasTarget (AbstractGameObject target) {
+		return hasTarget() && this.target.equals(target);
+	}
 
-     public void applyTo (OrthographicCamera camera) {
-             camera.position.x = position.x;
-             camera.position.y = position.y;
-             camera.zoom = zoom;
-             camera.update();
-     }
-
+	public void applyTo (OrthographicCamera camera) {
+		camera.position.x = position.x;
+		camera.position.y = position.y;
+		camera.zoom = zoom;
+		camera.update();
+	}
 }
--- core/src/com/rabbitrock/util/Constants.java
+++ core/src/com/rabbitrock/util/Constants.java
@@ -1,23 +1,39 @@
 package com.rabbitrock.util;
 
 public class Constants {
-	// Visible game world in 5 meters wide
+	// Visible game world is 5 meters wide
 		public static final float VIEWPORT_WIDTH = 5.0f;
-		
-		// Visible game world in 5 meters tall
+
+		// Visible game world is 5 meters tall
 		public static final float VIEWPORT_HEIGHT = 5.0f;
+
+		// GUI Width
+		public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+
+		// GUI Height
+		public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+
 		// Location of description file for texture atlas
-		public static final String TEXTURE_ATLAS_OBJECTS = "../android/assets/images/canyonbunny.pack";
+		public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
+		public static final String TEXTURE_ATLAS_UI = "images/canyonbunny-ui.pack";
+		public static final String TEXTURE_ATLAS_LIBGDX_UI = "images/uiskin.atlas";
+
+		// Location of description file for skins
+		public static final String SKIN_LIBGDX_UI = "images/uiskin.json";
+		public static final String SKIN_CANYONBUNNY_UI = "images/canyonbunny-ui.json";
+
 		// Location of image file for level 01
 		public static final String LEVEL_01 = "levels/level-01.png";
+
+		// Amount of extra lives at level start
+		public static final int LIVES_START = 3;
+
 		// Duration of feather power-up in seconds
 		public static final float ITEM_FEATHER_POWERUP_DURATION = 9;
-		public static final String TEXTURE_ATLAS_UI = 
-				"images/canyonbunny-ui.pack"; 
-		public static final String TEXTURE_ATLAS_LIBGDX_UI =  
-				"images/uiskin.atlas"; 
-		// Location of description file for skins  
-		public static final String SKIN_LIBGDX_UI =    
-				"images/uiskin.json"; 
-		public static final String SKIN_CANYONBUNNY_UI =      "images/canyonbunny-ui.json"; 
+
+		// Delay after game over
+		public static final float TIME_DELAY_GAME_OVER = 3;
+
+		// Game preferences file
+		public static final String PREFERENCES = "canyonbunny.prefs";
 }
